78321867e1696b7af30846e92ebb1502fe2e277b
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-07-17 13:14:19 +0100

    finished last functions, finished Readme file with screenshot and deployed URL

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,181 @@
 // Assignment Code- finds the button ID.
 var generateBtn = document.querySelector("#generate");
 
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var specialCharactersArray = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+var numericCharactersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-function getPassword () {
-  var chars = "0123456789abcdefghijklmnopqrstuvwxy!@$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var password = "";
+function getRandomElement(myArray) {
+  var randomIndex = Math.floor(Math.random() * myArray.length);
+  var randomElement = myArray[randomIndex];
+  return randomElement;
 }
-
-for (var i = 0; i< passwordLength; i++){
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
-
-}
-
-document.getElementById('password').value = password;
-
-
-//creating first alert
-
-
-window.alert("Hello, in a moment, you will create a strong password. Please click 'ok' to the following options to choose your password. Are you ready?");
-
-
-window.confirm("Do you want a special character to be selected?")
-
-
- if (window.confirm("Do you want to a lowercase letter to be selected?")) {window.open("Cancel", "Ok");
 
 //write generate password function
 
+function generatePassword() {
+  var pwdOptions = askPasswordPrompts();
+  console.log(pwdOptions);
+  var guaranteedCharacters = [];
+  var possibleCharacters = [];
+  var finalResult = [];
 
+  if (pwdOptions.lowerCase) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandomElement(lowerCasedCharacters));
+  }
 
+  if (pwdOptions.upperCase) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandomElement(upperCasedCharacters));
+  }
+
+  if (pwdOptions.specialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharactersArray);
+    guaranteedCharacters.push(getRandomElement(specialCharactersArray));
+  }
+
+  if (pwdOptions.numericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharactersArray);
+    guaranteedCharacters.push(getRandomElement(numericCharactersArray));
+  }
+
+  for (var i = 0; i < pwdOptions.length; i++) {
+    var possibleChar = getRandomElement(possibleCharacters);
+    finalResult.push(possibleChar);
+  }
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    finalResult[i] = guaranteedCharacters[i];
+  }
+  return finalResult.join("");
+}
+
+function askPasswordPrompts() {
+  var passwordLength = parseInt(
+    prompt("How many characters would you like in your password?")
+  );
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be 8 characters and 128 characters");
+    return null;
+  }
+
+  var hasUpperCase = confirm("Would you like upper case letters?");
+
+  var hasLowerCase = confirm("Would you like lower case letters?");
+
+  var hasSpecialCharacters = confirm("Would you like special characters?");
+
+  var hasNumericCharacters = confirm("Would you like numbers?");
+
+  if (
+    hasLowerCase === false &&
+    hasNumericCharacters === false &&
+    hasSpecialCharacters === false &&
+    hasUpperCase === false
+  ) {
+    alert("!");
+    return null;
+  }
+
+  var passwordPromptsObject = {
+    length: passwordLength,
+    upperCase: hasUpperCase,
+    lowerCase: hasLowerCase,
+    specialCharacters: hasSpecialCharacters,
+    numericCharacters: hasNumericCharacters,
+  };
+  return passwordPromptsObject;
+}
 
 // Write password to the #password input
 function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-console.log('password', password);
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  console.log("password", password);
 }
-
-
-
-
-
 
 /*Add event listener to generate button
 //targets generateButton (an object)- id is generatebtn. the event is clicking and writepassword function is executed.
 //dot calls a method- an action. */
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
--- README1.md
+++ README1.md
@@ -1,14 +1,15 @@
 ## Password Generator
 
-# Description 
-* A button is created which leads to prompts
-* Prompts ask user if they want to create a password of 8 characters, including uppercase, lowercase, symbols and special characters.
-* Loop runs
-* This then prints to the document page.
+# Description
+
+- A button is created which leads to prompts
+- Prompts ask user if they want to create a password of 8 characters, including uppercase, lowercase, symbols and special characters.
+- Loop runs
+- This then prints to the document page.
 
 # Link to deployed application
 
-[Link to deployed URL](https://laura-kam.github.io/AssignmentTwo/)
+[Link to deployed URL](https://laura-kam.github.io/GenerateMyPassword/)
 
 # Link to Git-hub repo.
 
@@ -16,14 +17,13 @@
 
 # Screenshot of website:
 
-![Screenshot of website](https://github.com/Laura-Kam/AssignmentTwo/issues/1#issue-1299932156)
-
+![Screenshot of website](https://github.com/Laura-Kam/GenerateMyPassword/issues/1#issue-1307078859)
 
 # Version history
 
-* 0.1 Character set added into a variable container
-* 0.2 
+- 0.1 Character set added into a variable container.
+- 0.2 Uploaded final working password generator. Pushed final commits.
 
 # License
 
-* MIT
+- MIT
